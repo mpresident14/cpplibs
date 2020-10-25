46510a82ba1f1f87b6a7119882ff38bb76d252eb
mpresident14 <mcprez10@gmail.com>
2020-10-25 02:37:21 -0700

    Implement binding to an instance.

--- src/injector/injector.hpp
+++ src/injector/injector.hpp
@@ -5,7 +5,9 @@
   int injector_dummy_var = injector::addCtor<ctorDecl>(__LINE__, __FILE__); \
   ctorDecl
 
+#include <any>
 #include <iostream>
+#include <memory>
 #include <stdexcept>
 #include <string>
 #include <unordered_map>
@@ -33,9 +35,19 @@ struct CtorInfo {
   size_t line;
   const char* filename;
 };
-
 unordered_map<string, CtorInfo> constructors;
-// unordered_map<string, InstanceInfo> constructors;
+
+enum class BindingType { INSTANCE, INSTANCE_PTR, CLASS, PROVIDER };
+struct Binding {
+  BindingType type;
+  std::any obj;
+};
+unordered_map<string, Binding> bindings;
+
+template <typename T>
+const char* getId() {
+  return typeid(decay_t<T>).name();
+}
 
 }  // namespace
 
@@ -55,15 +67,65 @@ int addCtor(size_t line, const char* filename) {
         + ctorTypes.classType);
   }
 
-  cout << p.first->first << endl;
-  cout << p.first->second.argTypes << endl;
-  cout << p.first->second.line << endl;
-  cout << p.first->second.filename << endl;
-
   return 0;
+}
+
+template <typename Bound, typename T>
+    requires same_as<Bound, T> || derived_from<Bound, T> void bindToInstance(T&& obj) {
+  if (!bindings.emplace(getId<T>(), Binding{ BindingType::INSTANCE, make_any<T>(forward<T>(obj)) })
+           .second) {
+    // TODO: Maybe use macro so that we can output line and file of original binding
+    throw runtime_error(string("Binding for type ").append(getId<T>()).append(" already exists."));
+  }
+}
+
+// std::any requires a copy-constructible type, so we offer bindToInstancePtr for
+// non-copy-constructible types
+template <typename T>
+requires is_copy_constructible_v<T> void bindToInstance(T&& obj) {
+  return bindToInstance<T, T>(forward<T>(obj));
+}
+
+template <typename Bound, typename T>
+    requires same_as<Bound, T> || derived_from<Bound, T> void bindToInstancePtr(unique_ptr<T>&& objPtr) {
+  if (!bindings
+           .emplace(
+               getId<T>(),
+               Binding{ BindingType::INSTANCE_PTR,
+                        make_any<shared_ptr<T>>(shared_ptr<T>(objPtr.release())) })
+           .second) {
+    // TODO: Maybe use macro so that we can output line and file of original binding
+    throw runtime_error(string("Binding for type ").append(getId<T>()).append(" already exists."));
+  }
+}
+
+template <typename T>
+void bindToInstancePtr(unique_ptr<T>&& objPtr) {
+  return bindToInstancePtr<T, T>(move(objPtr));
+}
+
+template <typename T>
+T& inject() {
+  auto iter = bindings.find(typeid(T).name());
+  if (iter == bindings.end()) {
+    throw runtime_error(string("No binding exists for type ").append(typeid(T).name()));
+  }
+
+  Binding& binding = iter->second;
+  switch (binding.type) {
+    case BindingType::INSTANCE:
+      return any_cast<T&>(binding.obj);
+    case BindingType::INSTANCE_PTR:
+      return *any_cast<shared_ptr<T>>(binding.obj);
+    default:
+      throw runtime_error("Unimplemented");
+  }
 }
 
 }  // namespace injector
 
+
+// TODO: Problem with injecting a raw ptr into ctor is that class doesn't know whether it those ptrs
+// were injected and need to be deleted.
 
 #endif  // INJECTOR_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,23 @@ class Foo {
 public:
   Foo() = default;
   INJECT(Foo(int n, char c)) {}
+  Foo(const Foo&) = delete;
+  Foo(Foo&&) = delete;
+
+  int a = 123456;
 };
 
 int main() {
-  Foo f;
+  int n = 1;
+  injector::bindToInstance(n);
+  injector::bindToInstancePtr(make_unique<string>("hello"));
+  int m = injector::inject<int>();
+  string& str = injector::inject<string>();
+  cout << m << endl;
+  cout << str << endl;
+  // Foo f;
+  injector::bindToInstancePtr(unique_ptr<Foo>(new Foo()));
+  Foo& foo = injector::inject<Foo>();
+  cout << foo.a << endl;
   return 0;
 }
