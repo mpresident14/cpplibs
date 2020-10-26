c6aa7e8b1411f7cb7d2104d5a461f3a719cd9a66
mpresident14 <mcprez10@gmail.com>
2020-10-26 03:21:10 -0700

    Improve unit testing interface.

--- src/injector/injector_test.cpp
+++ src/injector/injector_test.cpp
@@ -5,8 +5,7 @@
 #include <string>
 
 using namespace std;
-
-prez::UnitTest TESTER = prez::UnitTest::createTester();
+using namespace unit_test;
 
 static int BASE = 0;
 static int DERIVED = 1;
@@ -24,106 +23,92 @@ public:
   Derived(Derived&&) = delete;
 };
 
-void test_bindToInstance_sameType_success() {
+TEST(bindToInstance_sameType_success) {
   injector::clearBindings();
 
   injector::bindToInstance(make_shared<Base>());
   shared_ptr<Base> b = injector::inject<shared_ptr<Base>>();
 
-  TESTER.assertEquals(BASE, b->val);
+  assertEquals(BASE, b->val);
 }
 
-void test_bindToInstance_derivedType_success() {
+TEST(bindToInstance_derivedType_success) {
   injector::clearBindings();
 
   injector::bindToInstance<Base>(make_shared<Derived>());
   shared_ptr<Base> b = injector::inject<shared_ptr<Base>>();
 
-  TESTER.assertEquals(DERIVED, b->val);
+  assertEquals(DERIVED, b->val);
 }
 
-void test_bindToInstance_uniquePtr_throws() {
+TEST(bindToInstance_uniquePtr_throws) {
   injector::clearBindings();
 
   injector::bindToInstance<Base>(make_shared<Derived>());
 
-  string err = TESTER.assertThrows([]() { injector::inject<unique_ptr<Base>>(); });
-  TESTER.assertTrue(err.find("only a shared_ptr was bound") != string::npos);
+  string err = assertThrows([]() { injector::inject<unique_ptr<Base>>(); });
+  assertTrue(err.find("only a shared_ptr was bound") != string::npos);
 }
 
-void test_bindToUniqueProvider_sameType_success() {
+TEST(bindToUniqueProvider_sameType_success) {
   injector::clearBindings();
 
   injector::bindToProvider<int>([]() { return make_unique<int>(123456); });
   unique_ptr<int> n = injector::inject<unique_ptr<int>>();
 
-  TESTER.assertEquals(*n, 123456);
+  assertEquals(*n, 12356);
 }
 
-void test_bindToUniqueProvider_derivedType_success() {
+TEST(bindToUniqueProvider_derivedType_success) {
   injector::clearBindings();
 
   injector::bindToProvider<Base>([]() { return unique_ptr<Derived>(new Derived()); });
   unique_ptr<Base> b = injector::inject<unique_ptr<Base>>();
 
-  TESTER.assertEquals(DERIVED, b->val);
+  assertEquals(DERIVED, b->val);
 }
 
-void test_bindToUniqueProvider_injectSharedPtr_success() {
+TEST(bindToUniqueProvider_injectSharedPtr_success) {
   injector::clearBindings();
 
   injector::bindToProvider<Base>([]() { return make_unique<Base>(Derived()); });
   shared_ptr<Base> b = injector::inject<shared_ptr<Base>>();
 
-  TESTER.assertEquals(DERIVED, b->val);
+  assertEquals(DERIVED, b->val);
 }
 
-void test_bindToSharedProvider_sameType_success() {
+void bindToSharedProvider_sameType_success() {
   injector::clearBindings();
 
   injector::bindToProvider<int>([]() { return make_shared<int>(123456); });
   shared_ptr<int> n = injector::inject<shared_ptr<int>>();
 
-  TESTER.assertEquals(*n, 123456);
+  assertEquals(*n, 123456);
 }
 
-void test_bindToSharedProvider_derivedType_success() {
+TEST(bindToSharedProvider_derivedType_success) {
   injector::clearBindings();
 
   injector::bindToProvider<Base>([]() { return make_shared<Base>(Derived()); });
   shared_ptr<Base> b = injector::inject<shared_ptr<Base>>();
 
-  TESTER.assertEquals(DERIVED, b->val);
+  assertEquals(DERIVED, b->val);
 }
 
-void test_bindToSharedProvider_injectUniquePtr_throws() {
+TEST(bindToSharedProvider_injectUniquePtr_throws) {
   injector::clearBindings();
 
   injector::bindToProvider<Base>([]() { return make_shared<Base>(Derived()); });
 
-  string err = TESTER.assertThrows([]() { injector::inject<unique_ptr<Base>>(); });
-  TESTER.assertTrue(err.find("only a shared_ptr was bound") != string::npos);
+  string err = assertThrows([]() { injector::inject<unique_ptr<Base>>(); });
+  assertTrue(err.find("only a shared_ptr was bound") != string::npos);
 }
 
-void test_inject_noBinding_throws() {
+TEST(inject_noBinding_throws) {
   injector::clearBindings();
 
-  string err = TESTER.assertThrows([]() { injector::inject<shared_ptr<Base>>(); });
-  TESTER.assertTrue(err.find("No binding") != string::npos);
+  string err = assertThrows([]() { injector::inject<shared_ptr<Base>>(); });
+  assertTrue(err.find("No binding") != string::npos);
 }
 
-int main() {
-  test_bindToInstance_sameType_success();
-  test_bindToInstance_derivedType_success();
-  test_bindToInstance_uniquePtr_throws();
-  test_bindToUniqueProvider_sameType_success();
-  test_bindToUniqueProvider_derivedType_success();
-  test_bindToUniqueProvider_injectSharedPtr_success();
-  test_bindToSharedProvider_sameType_success();
-  test_bindToSharedProvider_derivedType_success();
-  test_bindToSharedProvider_injectUniquePtr_throws();
-  test_inject_noBinding_throws();
-}
-
-// TODO: Rewrite unit_tester to take in void -> void functions and run them, also add @Before and
-// @After functions (TESTER.setBefore(fn), TESTER.setAfter(fn), TESTER.run(fn1, fn2, ...))
+int main() { runTests(); }
--- src/testing/unit_test.hpp
+++ src/testing/unit_test.hpp
@@ -5,22 +5,30 @@
 #include <cstring>
 #include <exception>
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <type_traits>
 #include <unordered_map>
+#include <utility>
 
 #include <experimental/source_location>
 
-namespace prez {
+#define TEST(x)                                  \
+  void x();                                      \
+  int x##_dummy_var = unit_test::addTest(x, #x); \
+  void x()
 
-/*******************************************************
- * Check to see if a type can use the ostream operator
- ******************************************************/
+
+namespace {
+
+
+/*********************************************
+ * Check to see if a type can use operator<< *
+ *********************************************/
 template <typename T>
 struct is_printable {
-private:
   template <typename TT>
   static auto test(bool)
       -> decltype(std::declval<std::ostream&>() << std::declval<TT>(), std::true_type());
@@ -28,7 +36,6 @@ private:
   template <typename>
   static auto test(int) -> std::false_type;
 
-public:
   // Passing in true tries the bool parameter overload first.
   static constexpr bool value = decltype(is_printable::test<T>(true))::value;
 };
@@ -36,201 +43,189 @@ public:
 template <typename T>
 inline constexpr bool is_printable_v = is_printable<T>::value;
 
-/***********************
- * Actual testing class
- ***********************/
-class UnitTest {
-private:
-  UnitTest(const char* fileName) : in_{ fileName }, fileName_{ fileName } {}
 
-  void initTest(const char* testName) {
-    // Print results of previous test.
-    if (testName_) {
-      printResults();
+/**********************
+ * Tracking Variables *
+ **********************/
+
+// Per test
+size_t affirmsInTest_ = 0;
+size_t failuresInTest_ = 0;
+bool alreadyMarkedFailure_ = false;
+
+// Per file
+size_t testsFailed_ = 0;
+size_t totalTests_ = 0;
+size_t currentLine_ = 0;
+
+std::ifstream in_;
+std::unordered_map<size_t, std::string> prevLines_;
+std::vector<std::pair<std::function<void(void)>, const char*>> tests_;
+
+void initTest(const char* testName) {
+  std::string stars(strlen(testName) + 4, '*');
+
+  std::cout << stars << std::endl;
+  std::cout << "* " << testName << " *" << std::endl;
+  std::cout << stars << std::endl;
+
+  affirmsInTest_ = 0;
+  failuresInTest_ = 0;
+  alreadyMarkedFailure_ = false;
+  ++totalTests_;
+}
+
+// Per-test statistics
+void printTestResult() {
+  std::cout << "Passed " << affirmsInTest_ - failuresInTest_ << " / " << affirmsInTest_
+            << " affirmations."
+            << "\n\n"
+            << std::endl;
+}
+
+// Entire file statistics
+void summarizeResults() {
+  std::cout << "------------------------------------------------" << std::endl;
+  std::cout << "SUMMARY: Passed " << totalTests_ - testsFailed_ << " / " << totalTests_ << " tests."
+            << std::endl;
+
+  if (testsFailed_ == 0) {
+    std::cout << "Congratulations! All tests passed!" << std::endl;
+  }
+  std::cout << "------------------------------------------------" << std::endl;
+}
+
+
+}  // namespace
+
+
+
+/******************
+ * Public Methods *
+ ******************/
+namespace unit_test {
+
+
+void assertTrue(
+    bool statement,
+    const std::experimental::source_location& location =
+        std::experimental::source_location::current(),
+    std::string&& errMsg = "") {
+  ++affirmsInTest_;
+
+  size_t line = location.line();
+  std::string s;
+  // If we need to move forward to reach the affirm statement
+  if (currentLine_ < line) {
+    // Advance ifstream to the affirm line
+    for (size_t i = 0; i < line - currentLine_ - 1; ++i) {
+      in_.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    // Grab the affirm statement
+    getline(in_, s);
+    currentLine_ = line;
+    prevLines_[line] = s;
+  }
+  // If we have already gotten this line (e.g. affirm() in a loop)
+  else {
+    s = prevLines_[line];
+  }
+
+  if (!statement) {
+    ++failuresInTest_;
+    std::cout << "FAILURE: " << location.file_name() << ", line " << line << ": " << s << std::endl;
+    if (!errMsg.empty()) {
+      std::cout << errMsg << std::endl;
     }
 
-    std::string stars(strlen(testName) + 4, '*');
-
-    std::cout << stars << std::endl;
-    std::cout << "* " << testName << " *" << std::endl;
-    std::cout << stars << std::endl;
-
-    affirmsInTest_ = 0;
-    failuresInTest_ = 0;
-    alreadyMarkedFailure_ = false;
-    ++totalTests_;
-    testName_ = testName;
-  }
-
-  // Individual test statistics
-  void printResults() {
-    std::cout << "Passed " << affirmsInTest_ - failuresInTest_ << " / " << affirmsInTest_
-              << " affirmations."
-              << "\n\n"
-              << std::endl;
-  }
-
-  // Entire file statistics
-  void summarize() {
-    std::cout << "------------------------------------------------" << std::endl;
-    std::cout << "SUMMARY: Passed " << totalTests_ - testsFailed_ << " / " << totalTests_
-              << " tests." << std::endl;
-
-    if (testsFailed_ == 0) {
-      std::cout << "Congratulations! All tests passed!" << std::endl;
-    }
-    std::cout << "------------------------------------------------" << std::endl;
-  }
-
-  // Per test
-  size_t affirmsInTest_ = 0;
-  size_t failuresInTest_ = 0;
-  bool alreadyMarkedFailure_ = false;
-  const char* testName_ = nullptr;
-
-  // Per file
-  size_t testsFailed_ = 0;
-  size_t totalTests_ = 0;
-  size_t currentLine_ = 0;
-
-  std::ifstream in_;
-  std::unordered_map<size_t, std::string> prevLines_;
-  std::string fileName_;
-
-public:
-  static UnitTest createTester(const std::experimental::source_location& location =
-          std::experimental::source_location::current()) { return UnitTest(location.file_name()); }
-
-  ~UnitTest() {
-    // Need to print results of last test.
-    printResults();
-    summarize();
-  }
-  UnitTest(const UnitTest&) = delete;
-  UnitTest& operator=(const UnitTest&) = delete;
-
-  template <typename F>
-  std::string assertThrows(
-      const F& fn,
-      const std::experimental::source_location& location =
-          std::experimental::source_location::current()) {
-    try {
-      fn();
-      assertTrue(false, location);
-      return "";
-    } catch (std::exception& e) {
-      assertTrue(true, location);
-      return e.what();
+    // Update the total number of failed tests
+    if (!alreadyMarkedFailure_) {
+      alreadyMarkedFailure_ = true;
+      ++testsFailed_;
     }
   }
+}
 
-  template <typename F>
-  void assertThrowsWithMsg(
-      const F& fn,
-      const std::string& errMsg,
-      const std::experimental::source_location& location =
-          std::experimental::source_location::current()) {
-    try {
-      fn();
-      assertTrue(false, location);
-    } catch (std::exception& e) {
-      assertEquals(errMsg, std::string(e.what()), location);
-    }
+void assertFalse(
+    bool statement,
+    const std::experimental::source_location& location =
+        std::experimental::source_location::current()) {
+  assertTrue(!statement, location);
+}
+
+template <
+    typename T1,
+    typename T2,
+    std::enable_if_t<is_printable_v<T1> && is_printable_v<T2>, int> = 0>
+void assertEquals(
+    const T1& expected,
+    const T2& actual,
+    const std::experimental::source_location& location =
+        std::experimental::source_location::current()) {
+  std::ostringstream err;
+  bool b = expected == actual;
+  if (!b) {
+    err << "\t"
+        << "EXPECTED: " << expected << '\n';
+    err << "\t"
+        << "GOT:      " << actual;
   }
+  assertTrue(b, location, err.str());
+}
 
-  template <typename T1, typename T2>
-  void assertNotEqual(
-      const T1& obj,
-      const T2& actual,
-      const std::experimental::source_location& location =
-          std::experimental::source_location::current()) {
-    assertTrue(obj != actual, location);
+template <
+    typename T1,
+    typename T2,
+    std::enable_if_t<!is_printable_v<T1> || !is_printable_v<T2>, int> = 0>
+void assertEquals(
+    const T1& expected,
+    const T2& actual,
+    const std::experimental::source_location& location =
+        std::experimental::source_location::current()) {
+  assertTrue(expected == actual, location);
+}
+
+template <typename T1, typename T2>
+void assertNotEqual(
+    const T1& obj,
+    const T2& actual,
+    const std::experimental::source_location& location =
+        std::experimental::source_location::current()) {
+  assertTrue(obj != actual, location);
+}
+
+template <typename F>
+std::string assertThrows(
+    const F& fn,
+    const std::experimental::source_location& location =
+        std::experimental::source_location::current()) {
+  try {
+    fn();
+    assertTrue(false, location);
+    return "";
+  } catch (std::exception& e) {
+    assertTrue(true, location);
+    return e.what();
   }
+}
 
-  template <
-      typename T1,
-      typename T2,
-      std::enable_if_t<is_printable_v<T1> && is_printable_v<T2>, int> = 0>
-  void assertEquals(
-      const T1& expected,
-      const T2& actual,
-      const std::experimental::source_location& location =
-          std::experimental::source_location::current()) {
-    std::ostringstream err;
-    bool b = expected == actual;
-    if (!b) {
-      err << "\t"
-          << "EXPECTED: " << expected << '\n';
-      err << "\t"
-          << "GOT:      " << actual;
-    }
-    assertTrue(b, location, err.str());
+template <typename F>
+int addTest(F test, const char* name) {
+  tests_.emplace_back(test, name);
+  // Assigning this to a dummy variable so we can call it in global space
+  return 0;
+}
+
+void runTests() {
+  for (auto p : tests_) {
+    initTest(p.second);
+    p.first();
+    printTestResult();
   }
+  summarizeResults();
+}
 
-  template <
-      typename T1,
-      typename T2,
-      std::enable_if_t<!is_printable_v<T1> || !is_printable_v<T2>, int> = 0>
-  void assertEquals(
-      const T1& expected,
-      const T2& actual,
-      const std::experimental::source_location& location =
-          std::experimental::source_location::current()) {
-    assertTrue(expected == actual, location);
-  }
 
-  void assertFalse(
-      bool statement,
-      const std::experimental::source_location& location =
-          std::experimental::source_location::current()) {
-    assertTrue(!statement, location);
-  }
-
-  void assertTrue(
-      bool statement,
-      const std::experimental::source_location& location =
-          std::experimental::source_location::current(),
-      std::string&& errMsg = "") {
-    if (location.function_name() != testName_) {
-      initTest(location.function_name());
-    }
-
-    ++affirmsInTest_;
-
-    size_t line = location.line();
-    std::string s;
-    // If we need to move forward to reach the affirm statement
-    if (currentLine_ < line) {
-      // Advance ifstream to the affirm line
-      for (size_t i = 0; i < line - currentLine_ - 1; ++i) {
-        in_.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      }
-      // Grab the affirm statement
-      getline(in_, s);
-      currentLine_ = line;
-      prevLines_[line] = s;
-    }
-    // If we have already gotten this line (e.g. affirm() in a loop)
-    else {
-      s = prevLines_[line];
-    }
-
-    if (!statement) {
-      ++failuresInTest_;
-      std::cout << "FAILURE: " << fileName_ << ", line " << line << ": " << s << std::endl;
-      if (!errMsg.empty()) {
-        std::cout << errMsg << std::endl;
-      }
-
-      // Update the total number of failed tests
-      if (!alreadyMarkedFailure_) {
-        alreadyMarkedFailure_ = true;
-        ++testsFailed_;
-      }
-    }
-  }
-};
-
-}  // namespace prez
+}  // namespace unit_test
 
 #endif
