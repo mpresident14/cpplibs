008669be270168e53b288d57d1fb0233beff5f71
Miles President <mcprez10@gmail.com>
2021-02-15 20:36:45 -0800

    Make printing lib not susceptible to namespace bs.

--- src/misc/print_stuff.hpp
+++ src/misc/print_stuff.hpp
@@ -1,222 +1,128 @@
 #ifndef PREZ_MISC_PRINT_STUFF_HPP
 #define PREZ_MISC_PRINT_STUFF_HPP
 
-#include <array>
-#include <cstddef>
-#include <deque>
-#include <forward_list>
-#include <iostream>
-#include <list>
-#include <map>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
+#include <string_view>
 #include <tuple>
-#include <type_traits>
-#include <unordered_map>
-#include <unordered_set>
 #include <utility>
+#include <ostream>
+
+#include "src/misc/typing.hpp"
 
 namespace prez {
 namespace misc {
-template <typename T> std::string arrayToString(T* arr, size_t length);
-}
-} // namespace prez
-
-template <typename... Ts>
-std::ostream& operator<<(std::ostream& out, const std::tuple<Ts...>& t);
-
-template <typename T1, typename T2>
-std::ostream& operator<<(std::ostream& out, const std::pair<T1, T2>& p);
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::vector<T>& c);
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::deque<T>& c);
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::forward_list<T>& c);
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::list<T>& c);
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::set<T>& c);
-
-template <typename... Ts>
-std::ostream& operator<<(std::ostream& out, const std::unordered_set<Ts...>& c);
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::stack<T>& c);
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::queue<T>& c);
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::priority_queue<T>& c);
-
-template <typename T, int N>
-std::ostream& operator<<(std::ostream& out, const std::array<T, N>& c);
-
-template <typename K, typename V>
-std::ostream& operator<<(std::ostream& out, const std::unordered_map<K, V>& c);
-
-template <typename K, typename V>
-std::ostream& operator<<(std::ostream& out, const std::map<K, V>& c);
-
-template <typename K, typename V>
-std::ostream& operator<<(std::ostream& out,
-                         const std::unordered_multimap<K, V>& c);
-
-/********
- * IMPL *
- ********/
-
-namespace prez {
-namespace misc {
-template <typename T> std::string arrayToString(T* arr, size_t length) {
-  std::stringstream stream;
-  stream << '[';
-
-  for (size_t i = 0; i < length; ++i) {
-    stream << arr[i];
-    if (i != length - 1) {
-      stream << ", ";
-    }
-  }
-  stream << ']';
-  return stream.str();
-}
-} // namespace misc
-} // namespace prez
-
 namespace detail {
-template <typename Container>
-std::ostream& printContainerHelper(std::ostream& out, const Container& c) {
-  size_t length = c.size();
-  size_t i = 0;
-  out << '[';
 
-  for (const auto& item : c) {
-    out << item;
-    if (i != length - 1) {
-      out << ", ";
-    }
-    ++i;
-  }
-  return out << ']';
+
+template <typename Obj>
+concept MapIterable = requires(Obj obj) {
+  obj.cbegin()->first;
+  obj.cbegin()->second;
+};
+
+template <typename Obj>
+concept NonMapIterable = requires(Obj obj) {
+  obj.cbegin();
 }
+&&(!(MapIterable<Obj> || std::convertible_to<Obj, std::string_view>));
 
-template <typename Map>
-std::ostream& printMapHelper(std::ostream& out, const Map& c) {
-  size_t length = c.size();
-  size_t i = 0;
-  out << "{\n";
+template <typename Obj>
+concept Pair = requires(Obj obj) {
+  obj.first;
+  obj.second;
+};
 
-  for (const auto& keyVal : c) {
-    out << "\t(" << keyVal.first << ": " << keyVal.second << ')';
-    if (i != length - 1) {
-      out << ",\n";
-    }
-    ++i;
-  }
-  return out << "\n}";
-}
+template <typename Obj>
+concept Tuple = is_specialization_v<Obj, std::tuple>;
 
-template <size_t I, typename Tuple,
-          std::enable_if_t<I == std::tuple_size_v<Tuple>, int> = 0>
-void printTupleHelper(std::ostream&, const Tuple&) {}
-
-template <size_t I, typename Tuple,
-          std::enable_if_t<I == std::tuple_size_v<Tuple> - 1, int> = 0>
-void printTupleHelper(std::ostream& out, const Tuple& t) {
-  out << std::get<I>(t);
-}
-
-template <size_t I, typename Tuple,
-          std::enable_if_t<(I < std::tuple_size_v<Tuple> - 1), int> = 0>
-void printTupleHelper(std::ostream& out, const Tuple& t) {
-  out << std::get<I>(t) << ", ";
-  printTupleHelper<I + 1, Tuple>(out, t);
-}
 } // namespace detail
 
-template <typename T1, typename T2>
-std::ostream& operator<<(std::ostream& out, const std::pair<T1, T2>& p) {
-  return out << '(' << p.first << ", " << p.second << ')';
-}
-
-template <typename... Ts>
-std::ostream& operator<<(std::ostream& out, const std::tuple<Ts...>& t) {
-  out << '(';
-  detail::printTupleHelper<0, std::tuple<Ts...>>(out, t);
-  return out << ')';
-}
+using namespace detail;
 
 template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::vector<T>& c) {
-  return detail::printContainerHelper(out, c);
-}
+class OStreamable {
+public:
+  OStreamable(const T& thing) : thing_(thing) {}
+
+  template <typename U>
+  friend std::ostream& operator<<(std::ostream&, const OStreamable<U>&);
+
+private:
+  template <size_t I, typename Tuple, std::enable_if_t<I == std::tuple_size_v<Tuple>, int> = 0>
+  static void printTupleHelper(std::ostream&, const Tuple&) {}
+
+  template <size_t I, typename Tuple, std::enable_if_t<I == std::tuple_size_v<Tuple> - 1, int> = 0>
+  static void printTupleHelper(std::ostream& out, const Tuple& t) {
+    out << OStreamable<decltype(std::get<I>(t))>(std::get<I>(t));
+  }
+
+  template <size_t I, typename Tuple, std::enable_if_t<(I < std::tuple_size_v<Tuple> - 1), int> = 0>
+  static void printTupleHelper(std::ostream& out, const Tuple& t) {
+    out << OStreamable<decltype(std::get<I>(t))>(std::get<I>(t)) << ", ";
+    printTupleHelper<I + 1, Tuple>(out, t);
+  }
+
+  template <typename U = T>
+  requires MapIterable<U> void toStream(std::ostream& out) const {
+    size_t length = thing_.size();
+    size_t i = 0;
+    out << "{\n";
+
+    for (const auto& [key, val] : thing_) {
+      out << "  (" << OStreamable<decltype(key)>(key) << ": " << OStreamable<decltype(val)>(val)
+          << ')';
+      if (i != length - 1) {
+        out << ",\n";
+      }
+      ++i;
+    }
+    out << "\n}";
+  }
+
+  template <typename U = T>
+  requires NonMapIterable<U> void toStream(std::ostream& out) const {
+    size_t length = thing_.size();
+    size_t i = 0;
+    out << '[';
+
+    for (const auto& item : thing_) {
+      out << OStreamable<decltype(item)>(item);
+      if (i != length - 1) {
+        out << ", ";
+      }
+      ++i;
+    }
+    out << ']';
+  }
+
+  template <typename U = T>
+  requires Pair<U> void toStream(std::ostream& out) const {
+    out << '(' << OStreamable<decltype(thing_.first)>(thing_.first) << ", "
+        << OStreamable<decltype(thing_.second)>(thing_.second) << ')';
+  }
+
+  template <typename... Args, typename U = T>
+  requires Tuple<T> void toStream(std::ostream& out) const {
+    out << '(';
+    printTupleHelper<0>(out, thing_);
+    out << ')';
+  }
+
+  template <typename U = T>
+  requires(!(MapIterable<U> || NonMapIterable<U> || Pair<U> || Tuple<T>)) void toStream(
+      std::ostream& out) const {
+    out << thing_;
+  }
+
+
+  const T& thing_;
+};
 
 template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::deque<T>& c) {
-  return detail::printContainerHelper(out, c);
-}
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::forward_list<T>& c) {
-  return detail::printContainerHelper(out, c);
-}
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::list<T>& c) {
-  return detail::printContainerHelper(out, c);
-}
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::set<T>& c) {
-  return detail::printContainerHelper(out, c);
+std::ostream& operator<<(std::ostream& out, const OStreamable<T>& ostreamable) {
+  return (ostreamable.toStream(out), out);
 }
 
-// TODO: Do param pack for all containers
-template <typename... Ts>
-std::ostream& operator<<(std::ostream& out,
-                         const std::unordered_set<Ts...>& c) {
-  return detail::printContainerHelper(out, c);
-}
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::stack<T>& c) {
-  return detail::printContainerHelper(out, c);
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::queue<T>& c) {
-  return detail::printContainerHelper(out, c);
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::priority_queue<T>& c) {
-  return detail::printContainerHelper(out, c);
-}
-
-template <typename T, int N>
-std::ostream& operator<<(std::ostream& out, const std::array<T, N>& c) {
-  return detail::printContainerHelper(out, c);
-}
-
-template <typename K, typename V>
-std::ostream& operator<<(std::ostream& out, const std::unordered_map<K, V>& c) {
-  return detail::printMapHelper(out, c);
-}
-
-template <typename K, typename V>
-std::ostream& operator<<(std::ostream& out, const std::map<K, V>& c) {
-  return detail::printMapHelper(out, c);
-}
-
-template <typename K, typename V>
-std::ostream& operator<<(std::ostream& out,
-                         const std::unordered_multimap<K, V>& c) {
-  return detail::printMapHelper(out, c);
-}
+} // namespace misc
+} // namespace prez
 
 #endif
