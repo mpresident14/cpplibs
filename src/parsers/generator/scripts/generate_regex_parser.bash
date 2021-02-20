b0159b8dfb13569316dc9f514e82b85a8824cb17
Miles President <mcprez10@gmail.com>
2021-02-19 23:02:18 -0800

    Update regex_parser generator script.

--- src/parsers/generator/scripts/generate_regex_parser.bash
+++ src/parsers/generator/scripts/generate_regex_parser.bash
@@ -1,11 +1,9 @@
 #!/bin/bash
 
+PARSER_DIR="src/parsers/generator/"
 
-INCLUDE_DIR="src/parsers/generator/"
-PARSER_DIR=$(pwd)/"$INCLUDE_DIR"
-
-bazel build //src/parsers/generator:parsegen
 cp "$PARSER_DIR"/regex_parser.hpp "$PARSER_DIR"/obsolete/regex_parser_backup.hpp
 cp "$PARSER_DIR"/regex_parser.cpp "$PARSER_DIR"/obsolete/regex_parser_backup.cpp
-bazel run //src/parsers/generator:parsegen -- -g "$PARSER_DIR"/regex.pgen -d "$PARSER_DIR" -i "$INCLUDE_DIR"  -n regex_parser
+make parsegen
+../bin/parsegen -g "$PARSER_DIR"/regex.pgen -d "$PARSER_DIR" -i "$PARSER_DIR"  -n regex_parser
 clang-format -i "$PARSER_DIR"/regex_parser.hpp "$PARSER_DIR"/regex_parser.cpp
