7d5e5159bca5014cd53fb97b638cbdc79177096a
Miles President <mcprez10@gmail.com>
2023-02-07 01:36:52 -0800

    [HTTP Server] Add json accessors and tests.

--- src/webserver/json/json.hpp
+++ src/webserver/json/json.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <memory>
+#include <optional>
 #include <string>
 #include <string_view>
 #include <unordered_map>
@@ -58,9 +59,24 @@ protected:
   void to_stream(std::ostream& out) const override { out << value_; }
 };
 
+class JsonObject;
 class JsonArray final : public JsonElement {
 public:
   JsonArray(std::vector<std::unique_ptr<JsonElement>>&& elems) : elems_(std::move(elems)) {}
+
+  bool get_bool(size_t index) const { return get_element<JsonBool>(index, "bool")->value_; }
+
+  const std::string& get_string(size_t index) const {
+    return get_element<JsonString>(index, "string")->value_;
+  }
+
+  long get_long(size_t index) const { return get_element<JsonLong>(index, "long")->value_; }
+
+  const JsonArray& get_array(size_t index) const { return *get_element<JsonArray>(index, "array"); }
+
+  const JsonObject& get_object(size_t index) const {
+    return *get_element<JsonObject>(index, "array");
+  }
 
   std::vector<std::unique_ptr<JsonElement>> elems_;
 
@@ -79,6 +95,16 @@ protected:
     }
     out << ']';
   }
+
+private:
+  template <typename T>
+  T* get_element(size_t index, const char* type) const {
+    T* element = dynamic_cast<T*>(elems_.at(index).get());
+    if (element == nullptr) {
+      throw std::invalid_argument("JsonElement at " + std::to_string(index) + " is not a " + type);
+    }
+    return element;
+  }
 };
 
 class JsonObject final : public JsonElement {
@@ -91,7 +117,35 @@ public:
     }
   }
 
+  bool get_bool(const std::string& key) { return get_element<JsonBool>(key, "bool")->value_; }
+
+  const std::string& get_string(const std::string& key) const {
+    return get_element<JsonString>(key, "string")->value_;
+  }
+
+  long get_long(const std::string& key) const { return get_element<JsonLong>(key, "long")->value_; }
+
+  const JsonArray& get_array(const std::string& key) const {
+    return *get_element<JsonArray>(key, "array");
+  }
+
+  const JsonObject& get_object(const std::string& key) const {
+    return *get_element<JsonObject>(key, "array");
+  }
+
+
   std::unordered_map<std::string, std::unique_ptr<JsonElement>> entries_;
+
+private:
+  template <typename T>
+  T* get_element(const std::string& key, const char* type) const {
+    T* element = dynamic_cast<T*>(entries_.at(key).get());
+    if (element == nullptr) {
+      throw std::invalid_argument("JsonElement at " + key + " is not a " + type);
+    }
+    return element;
+  }
+
 
 protected:
   void to_stream(std::ostream& out) const override {
